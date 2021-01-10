0937b59d724017de4f3f72b1866f008c047e6947
Leonardo <oliveirabalsa2@gmail.com>
2021-01-10 17:01:21 -0300

    test

--- src/AuthModule/auth.guard.ts
+++ src/AuthModule/auth.guard.ts
@@ -7,6 +7,7 @@ import {
 } from '@nestjs/common';
 import * as jwt from 'jsonwebtoken';
 
+//cvawd
 @Injectable()
 export class AuthGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
